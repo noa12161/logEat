3e9f80f4315fe7358b07d5d507804651a774aae9
noa12161 <noa12161@gmail.com>
2022-04-18 23:16:58 +0900

    v1

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ import searchRoutes from "./routes/searchFood.js";
 import userRouter from "./routes/user.js";
 
 const app = express();
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URL)
   .then(console.log("connected to DB"))
